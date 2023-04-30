8c5355503ad15b3a938ab512fcc5cda28a1efc1b
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 14:22:53 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,23 +5,30 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('unattended-upgrades'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("unattended-upgrades"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file,user,group,mode', [
-  ('20auto-upgrades', 'root', 'root', 0o644),
-  ('50unattended-upgrades', 'root', 'root', 0o644),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode",
+    [
+        ("20auto-upgrades", "root", "root", 0o644),
+        ("50unattended-upgrades", "root", "root", 0o644),
+    ],
+)
 def test_config_files_exist(host, file, user, group, mode):
-    config = host.file('/etc/apt/apt.conf.d/' + file)
+    config = host.file("/etc/apt/apt.conf.d/" + file)
     assert config.exists
     assert config.is_file
     assert config.user == user
